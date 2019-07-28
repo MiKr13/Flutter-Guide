ba294b66ed72aa2a6634b5ebd962355e8f68a195
Mihir Kumar <mihirpandey.13@gmail.com>
2019-07-28 16:54:06 +0530

    Footer card, dialog, url launcher, long press

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,23 +31,66 @@ class MyApp extends StatelessWidget {
 class MyFooter extends StatelessWidget {
   const MyFooter({Key key}) : super(key: key);
 
+  Future<void> _support(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Author Credit'),
+          content: SingleChildScrollView(
+            child: ListTile(
+              leading: Icon(Icons.sentiment_very_satisfied),
+              title: Text('Developed by: Mihir Kumar'),
+              subtitle: new RichText(
+                text: new TextSpan(
+                  text: 'Support me at Github',
+                  style: new TextStyle(color: Colors.blue),
+                  recognizer: new TapGestureRecognizer()
+                    ..onTap = () async {
+                      const url = 'https://github.com/MiKr13/';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                ),
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Okay!'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        color: Color.fromRGBO(30, 144, 255, 0.1),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.code),
-              title: Text('Flutter Guide'),
-              subtitle: Text('This is created to learn flutter ASAP'),
-            ),
-          ],
+        child: Card(
+      color: Color.fromRGBO(30, 144, 255, 0.1),
+      child: InkWell(
+        splashColor: Colors.black.withAlpha(20),
+        onLongPress: () {
+          _support(context);
+        },
+        child: Container(
+          child: ListTile(
+            leading: Icon(Icons.code),
+            title: Text('Flutter Guide'),
+            subtitle: Text('This is created to learn flutter ASAP'),
+          ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
 
