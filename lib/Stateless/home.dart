fd1cf2528b99692454eb561cea2be8dce38ccb04
Mihir Kumar <mihirpandey.13@gmail.com>
2019-08-15 08:31:01 +0530

    Horizontal and Vertical ListView implemented

--- lib/Stateless/home.dart
+++ lib/Stateless/home.dart
@@ -6,17 +6,38 @@ import 'package:XpenseTracker/Stateless/footer.dart';
 class MyHomePage extends StatelessWidget {
   final String title;
   final Function _changeTheme;
+  final warningLimit = 800;
   final List<Transaction> transactions = [
     Transaction(
       id: 't1',
-      title: 'New shoes',
+      title: 'Hrx shoes',
       description: 'A new shoes for Friday party',
-      count: 1,
+      count: 2,
       costPerItem: 1250,
-      total: Transaction.calcTotal(1250, 1),
+      total: Transaction.calcTotal(1250, 2),
       date: DateTime.now(),
-    )
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Hrx shoes',
+      description: 'A new shoes for Friday party',
+      count: 2,
+      costPerItem: 200,
+      total: Transaction.calcTotal(200, 2),
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Hrx shoes',
+      description: 'A new shoes for Friday party',
+      count: 2,
+      costPerItem: 1250,
+      total: Transaction.calcTotal(1250, 2),
+      date: DateTime.now(),
+    ),
   ];
+
+  final List<int> console = [1, 2, 3, 4, 5];
 
   MyHomePage(this.title, this._changeTheme);
 
@@ -29,40 +50,105 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Chart!'),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                // Where the linear gradient begins and ends
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                stops: [0.1, 0.5, 0.7, 0.95],
+                colors: [
+                  Colors.yellow[200],
+                  Colors.cyan[600],
+                  Colors.cyan[400],
+                  Colors.yellow[100],
+                ],
+              ),
             ),
-          ),
-          ListView(
-            reverse: true,
-            children: transactions.map((tx) {
-              return Card(
-                child: Column(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        text: tx.title,
-                        style: DefaultTextStyle.of(context).style,
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: 'bold',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: ' world!'),
-                        ],
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+            height: MediaQuery.of(context).size.height * 0.30,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: console.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          console[index].toString(),
+                          style: TextStyle(color: Colors.white, fontSize: 36.0),
+                        ),
                       ),
                     ),
-                    ListTile(
-                      leading: Icon(Icons.label),
-                      title: Text('Two-line ListTile'),
-                      subtitle: Text('Here is a second line'),
-                      trailing: Icon(Icons.more_vert),
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
+                  ),
+                );
+              },
+            ),
+          ),
+          Divider(height: 5.0),
+          Flexible(
+            child: ListView(
+              children: transactions.map((tx) {
+                var date = tx.date.toLocal().toString().split(" ");
+                return Card(
+                  margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                  child: Column(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          text: '${tx.count} X ',
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.label,
+                          color: Colors.yellow[400],
+                        ),
+                        isThreeLine: true,
+                        title: RichText(
+                          text: TextSpan(
+                            text: 'Total cost: ',
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: '${tx.total}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: tx.total > warningLimit
+                                      ? Colors.red
+                                      : Colors.green,
+                                ),
+                              ),
+                              TextSpan(text: ' ₹'),
+                            ],
+                          ),
+                        ),
+                        subtitle: Text(
+                          '${tx.description}\nOn ${date[0]} at ${date[1].split(":")[0]}:${date[1].split(":")[1]}\nCost Per Item: ${tx.costPerItem}',
+                          overflow: TextOverflow.fade,
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.more_vert),
+                          tooltip: 'More',
+                          onPressed: () async {},
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ),
         ],
       ),
